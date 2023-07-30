35ac638d56fe24be9934fffcf23bcf19846cdbb2
Mobasshir Imam <imobasshirimam@gmail.com>
2023-07-30 17:03:49 +0530

    done

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BlogService } from 'src/app/services/blog.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  constructor(private blogSer: BlogService) { }
+
+  ngOnInit(): void {
+    this.getBlogs();
+  }
+
+  data!: Array<any>;
+
+  getBlogs(): any {
+
+    this.blogSer.getAllBlogs().subscribe({
+      next: (res) => {
+        console.log(res);
+        this.data = res;
+        
+        // console.log(this.data[0]);
+
+      },
+      error: (err) => console.log(err),
+    });
+  }
 }
